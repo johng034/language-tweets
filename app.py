078e762a529a89d7a66818c8f44989ea50748232
johng034 <67410822+johng034@users.noreply.github.com>
2022-01-19 22:41:31 -0800

    url took too long to load. adding path to local files

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-from io import BytesIO
-import requests
+# from io import BytesIO
+# import requests
+# import nltk
 
 st.set_page_config(page_title='Common Words')
 
@@ -18,8 +19,10 @@ st.markdown("""
 df = pd.read_csv('./data/cleaned_data.csv')
 
 wordcloud_urls = {
-    'French': 'https://github.com/johng034/language-tweets/blob/master/wordclouds/French_wordcloud.png?raw=true',
-    'German': 'https://github.com/johng034/language-tweets/blob/master/wordclouds/german_wordcloud.png?raw=true'
+    # 'French': 'https://github.com/johng034/language-tweets/blob/master/wordclouds/French_wordcloud.png?raw=true',
+    # 'German': 'https://github.com/johng034/language-tweets/blob/master/wordclouds/german_wordcloud.png?raw=true',
+    'French': './wordclouds/French_wordcloud.png',
+    'German': './wordclouds/german_wordcloud.png'
 }
 
 # Dropdown menu to choose language
@@ -27,6 +30,11 @@ language = st.selectbox('Select a language', options=df.columns)
 
 # Slider to choose the number of words
 num_of_words = st.slider('Number of Words', min_value=10, max_value=500, value=100, step=10)
+
+
+# Remove stop words
+# nltk.corpus.stopwords
+
 
 # Read the file containing common words data
 language_df = pd.read_csv(f'./data/common_words/{language}_common.csv')
@@ -38,9 +46,9 @@ language_df.set_index(np.arange(1,language_df.shape[0]+1), inplace=True)
 
 # Show word cloud
 with st.expander("See Word Cloud"):
-    response = requests.get(wordcloud_urls[language])
-    image = Image.open(BytesIO(response.content))
-    # image = Image.open(f'./wordclouds/{language}_wordcloud.png')
+    # response = requests.get(wordcloud_urls[language])
+    # image = Image.open(BytesIO(response.content))
+    image = Image.open(wordcloud_urls[language])
     st.image(image, caption=f'{language} Word Cloud')
 
 # Print table
