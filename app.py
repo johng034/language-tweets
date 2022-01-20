17b0f4890b3465134d6e3265f15b0df8e8e24a13
johng034 <67410822+johng034@users.noreply.github.com>
2022-01-20 12:58:10 -0800

    added three more languages to choose from

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-# from io import BytesIO
-# import requests
-# import nltk
 
 st.set_page_config(page_title='Common Words')
 
@@ -19,37 +16,30 @@ st.markdown("""
 df = pd.read_csv('./data/cleaned_data.csv')
 
 wordcloud_urls = {
-    # 'French': 'https://github.com/johng034/language-tweets/blob/master/wordclouds/French_wordcloud.png?raw=true',
-    # 'German': 'https://github.com/johng034/language-tweets/blob/master/wordclouds/german_wordcloud.png?raw=true',
-    'French': './wordclouds/French_wordcloud.png',
-    'German': './wordclouds/german_wordcloud.png'
+    'German' : './wordclouds/german_wordcloud.png',
+    'French' : './wordclouds/French_wordcloud.png',
+    'Italian': './wordclouds/Italian_wordcloud.png',
+    'Spanish': './wordclouds/Spanish_wordcloud.png',
+    'English': './wordclouds/English_wordcloud.png'
 }
 
 # Dropdown menu to choose language
-language = st.selectbox('Select a language', options=df.columns)
+language = st.selectbox('Select a language', options=df.columns.sort_values())
 
 # Slider to choose the number of words
-num_of_words = st.slider('Number of Words', min_value=10, max_value=500, value=100, step=10)
-
-
-# Remove stop words
-# nltk.corpus.stopwords
-
+num_of_words = st.slider('Number of Words', min_value=10, max_value=500, value=30, step=10)
 
 # Read the file containing common words data
 language_df = pd.read_csv(f'./data/common_words/{language}_common.csv')
-# Set index to start at 1
-language_df.set_index(np.arange(1,language_df.shape[0]+1), inplace=True)
-
-## TODO
-# Add a download button to save the data as a csv/xlsx file
+language_df.set_index(np.arange(1,language_df.shape[0]+1), inplace=True)  # Index start at 1
 
 # Show word cloud
 with st.expander("See Word Cloud"):
-    # response = requests.get(wordcloud_urls[language])
-    # image = Image.open(BytesIO(response.content))
     image = Image.open(wordcloud_urls[language])
     st.image(image, caption=f'{language} Word Cloud')
+
+## TODO
+# Add a download button to save the data as a csv/xlsx file
 
 # Print table
 st.header(f'Top {num_of_words} Most Common {language} Words')
